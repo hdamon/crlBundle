b4b653a6daee8c21b52c6b46b22a41dbf68b094a
Damon Hyde <damon.hyde@childrens.harvard.edu>
2018-06-21 18:35:56 -0400

    Updated README

---
+++ README.md
@@ -0,0 +1,3 @@
+# crlBundle : A collection of packages for EEG Source Localization
+
+crlBundle collects multiple Matlab packages into a single repository.
--- addCRLBundlePath.m
+++ addCRLBundlePath.m
@@ -1,4 +1,10 @@
+% Configure Matlab Path to Include the full crlBundle
 [currDir,~,~] = fileparts(mfilename('fullpath'));
-addpath(genpath(currDir));
+
+addpath(fullfile(currDir,'/labelledArray'));
+addpath(fullfile(currDir,'/MatTSA'));
+addpath(fullfile(currDir,'/guiTools'));
+addpath(fullfile(currDir,'/crlEEG'));
+
 
 
