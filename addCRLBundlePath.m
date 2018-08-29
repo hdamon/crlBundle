a1aca83916aa8dae688bcd8a42b4c0b0e649e077
Damon Hyde <damon.hyde@childrens.harvard.edu>
2018-08-29 18:34:38 -0400

    Updating the bungle

--- addCRLBundlePath.m
+++ addCRLBundlePath.m
@@ -1,6 +1,7 @@
 % Configure Matlab Path to Include the full crlBundle
 [currDir,~,~] = fileparts(mfilename('fullpath'));
 
+addpath(fullfile(currDir,'/crlBase'));
 addpath(fullfile(currDir,'/labelledArray'));
 addpath(fullfile(currDir,'/MatTSA'));
 addpath(fullfile(currDir,'/guiTools'));
